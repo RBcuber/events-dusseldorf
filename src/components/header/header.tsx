106045120b3f4c68fede9d8b688410a05d04804a
KonykOlha <OlhaKonyk@web.de>
2025-11-08 00:39:01 +0100

    added /events/page.tsx; modified: global.css, layout.tsx, header.tsx; created /app/not-found.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <div>Header</div>
-    );
+  return (
+    <div>
+      <nav className="bg-accent p-4">
+        <div className="flex space-x-6 justify-center">
+          <Link href={"/profile"}>Profile</Link>
+          <Link href={"/events"}>Events</Link>
+          <Link href={"/create/new"}>Admin / Create event</Link>
+          {/* <Link href={"/about"}>About us</Link>
+          <Link href={"/faq"}>FAQ</Link> */}
+          <Link href={"/sign-in"}>Sign in / Sign out</Link>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
