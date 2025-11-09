1f1b47e3f34e3c43f96f5bf87ea1b68b83e220b7
KonykOlha <OlhaKonyk@web.de>
2025-11-09 16:14:08 +0100

    add createEventButton and signUpButton to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,14 +3,24 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div>
-      <nav className="bg-accent p-4">
+      <nav className="bg-white p-4">
         <div className="flex space-x-6 justify-center">
-          <Link href={"/profile"}>Profile</Link>
+          <Link href={"/home"}>Home</Link>
           <Link href={"/events"}>Events</Link>
-          <Link href={"/create/new"}>Admin / Create event</Link>
-          {/* <Link href={"/about"}>About us</Link>
-          <Link href={"/faq"}>FAQ</Link> */}
-          <Link href={"/sign-in"}>Sign in / Sign out</Link>
+          <Link href={"/about"}>About</Link>
+          <Link href={"/faq"}>FAQ</Link>
+          <button
+            type="button"
+            className="bg-white text-violet-600 border-2 px-3 py-1.5 rounded-lg"
+          >
+            <Link href={"/events/new"}>Create event</Link>
+          </button>
+          <button
+            type="button"
+            className="bg-violet-600 text-white border-none px-3 py-1.5 rounded-lg"
+          >
+            <Link href={"/sign-up"}>Sign up</Link>
+          </button>
         </div>
       </nav>
     </div>
