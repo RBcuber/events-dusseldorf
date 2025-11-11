12d5453f6d81b1aaaa7925c668de706b6efbaf27
RBcuber <igor.kirichenko.07@gmail.com>
2025-11-11 10:02:33 +0100

    change events[id].page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header/header";
-import Footer from "../components/footer/footer"; // ⬅️ добавляем импорт футера
+import Footer from "../components/footer/footer"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,13 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen antialiased`}
       >
-        {/* Header — сверху на всех страницах */}
         <Header />
-
-        {/* Основной контент */}
         <main className="flex-grow">{children}</main>
-
-        {/* Footer — снизу на всех страницах */}
         <Footer />
       </body>
     </html>
